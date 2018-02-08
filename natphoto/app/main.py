857470e8af4b1c69461ed18f378839397dd48ea7
Jeff Bell <jeff@jhbell.com>
2018-02-07 21:54:15 -0600

    Add and render a simple html page

--- natphoto/app/main.py
+++ natphoto/app/main.py
@@ -1,9 +1,15 @@
-from flask import Flask
+"""
+main.py
+natphoto
+"""
+
+from flask import Flask, render_template
+
 app = Flask(__name__)
 
 @app.route("/")
-def hello():
-    return "TONY TIME YEEEHAWWWW!"
+def main():
+    return render_template("test.html")
 
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0")
