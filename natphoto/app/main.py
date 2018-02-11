0e2a46a90f999a5faec809243d87e3ba93f65630
Benjamin Johnson <bencj96@gmail.com>
2018-02-11 12:21:46 -0600

    Added simple location and tag filter functions for getting images from Flickr

--- natphoto/app/main.py
+++ natphoto/app/main.py
@@ -5,11 +5,41 @@ natphoto
 
 from flask import Flask, render_template
 
+import flickrapi
+api_key = "907cb3aabdbf0aefbaf5a0616e63d896"
+api_secret = "106bf23144feb125"
+flickr = flickrapi.FlickrAPI(api_key, api_secret)
+
 app = Flask(__name__)
+
+# Consider importing datetime and using timedelta for
+# only photos within a certain time delta TODO
 
 @app.route("/")
 def main():
     return render_template("mainpage.html")
+
+@app.route("/photos/<location>")
+def show_photo_by_location(location):
+    photo_titles = []
+    for photo in flickr.walk(tag_mode='all',
+        location= '%s' % str(location),
+        min_taken_date='2017-11-10'):
+        photo_titles.append(photo.get('title'))
+        if len(photo_titles) > 10:
+            break        
+    return str(photo_titles)
+
+@app.route("/photos/tag/<tag>")
+def show_photo_by_tag(tag):
+    photo_titles = []
+    for photo in flickr.walk(tag_mode='all',
+        tags= '%s' % str(tag),
+        min_taken_date= '2017-12-20'):
+        photo_titles.append(photo.get('title'))
+        if len(photo_titles) > 10:
+            break
+    return str(photo_titles)
 
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0")
