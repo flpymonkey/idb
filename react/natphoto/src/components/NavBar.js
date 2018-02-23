af8ebd7e6c1246b1ca3cf256648d12cad5b0fbe3
Benjamin Johnson <bencj96@gmail.com>
2018-02-22 19:16:53 -0600

    added React navbar (test)

---
+++ react/natphoto/src/components/NavBar.js
@@ -0,0 +1,15 @@
+import React, {Component} from "react";
+
+export class NavBar extends Component {
+	render {
+		return (
+			<ul class = "navbar">
+		  	<a href="/about" class = "navList">About</a>
+		    <a href="/cameras" class = "navList">Cameras</a>
+		    <a href="/photos" class = "navList">Photos</a>
+		    <a href="/parks" class = "navList">Parks</a>
+		    <a href="/" class = "navList">Home</a>
+		    <a id="logo" href="/"><span>NatPhoto</span><i class="material-icons">landscape</i></a>
+			</ul>);
+	}
+}
--- react/natphoto/src/index.js
+++ react/natphoto/src/index.js
@@ -4,5 +4,10 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// Our components
+import NavBar from './components/NavBar.js'
+
+
+
+ReactDOM.render(<NavBar />, document.getElementById('root'));
 registerServiceWorker();
