a4adf5515ebb3c8a30b0a18ec9d632ba0492a5d1
Benjamin Johnson <bencj96@gmail.com>
2018-02-23 17:37:31 -0600

    Importing bootstrap into React app

--- react/natphoto/src/index.js
+++ react/natphoto/src/index.js
@@ -4,6 +4,8 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
+import 'bootstrap/dist/css/bootstrap.css';
+
 // Our components
 import NavBar from './components/NavBar.js'
 
