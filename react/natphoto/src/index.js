f118ae98233bb80e060fa6f1182dc39c5e983980
tonydenapoli <tdenapoli1@gmail.com>
2018-02-23 17:51:55 -0600

    fit navbar in app.js

--- react/natphoto/src/index.js
+++ react/natphoto/src/index.js
@@ -3,13 +3,10 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
-
 import 'bootstrap/dist/css/bootstrap.css';
 
 // Our components
 import NavBar from './components/NavBar.js'
 
-
-
-ReactDOM.render(<NavBar />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
