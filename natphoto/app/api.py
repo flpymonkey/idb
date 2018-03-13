12472df9a5a69ee2660b617e55c9bec5968a396b
Benjamin Johnson <bencj96@gmail.com>
2018-03-13 14:48:20 -0500

    Added API endpoints for all parks and a park by name

--- natphoto/app/api.py
+++ natphoto/app/api.py
@@ -1,10 +1,45 @@
 from sqlalchemy import create_engine, MetaData
 from sqlalchemy.sql import select
+import json
 
+from flask import Flask
+from flask_restful import reqparse, abort, Api, Resource
+
+# Path to the database credentials file
+database_creds = '../idb_scraper/dbinfo.txt'
+
+# Flask API setup
+app = Flask(__name__)
+api = Api(app)
+
+##
+## Park
+##
+# shows a single park
+class Park(Resource):
+    def get(self, park_name):
+        result = handler.get_park(park_name)
+        return [dict(r) for r in result]
+
+# shows a list of all parks
+class ParkList(Resource):
+    def get(self):
+        result = handler.get_parks()
+        row = result.fetchone() #use fetchone() because the query returns lots of rows
+        results=[]
+        while row is not None:
+            results.append(dict(row))
+            row = result.fetchone()
+        return results
+
+##
+## DataHandler
+##
+# A general class to access data in the database
 class DataHandler (object):
     def __init__(self, dbfile):
         '''
-        Initializes a PhotoHandler object to make calls to the database
+        Initializes a DataHandler object to make calls to the database
         dbfile: a string file path to the file that contains database login credentials
         '''
         engine_string = None
@@ -19,7 +54,48 @@ class DataHandler (object):
         self.metadata.reflect(bind=self.engine)
 
     def get_parks(self):
+        '''
+        Get all parks in the database
+        '''
         parks_table = self.metadata.tables['parks']
         sel = select([parks_table])
         result = self.connection.execute(sel)
         return result
+
+    def get_park(self, name):
+        '''
+        Get a single park from the database by name
+        '''
+        parks_table = self.metadata.tables['parks']
+        sel = select([parks_table]).where(parks_table.c.name == name)
+        result = self.connection.execute(sel)
+        return result
+
+    def get_photos(self):
+        '''
+        Get all photos from the database
+        '''
+        photos_table = self.metadata.tables['photos']
+        sel = select([photos_table])
+        result = self.connection.execute(sel)
+        return result
+
+    def get_cameras(self):
+        '''
+        Get all cameras from the database
+        '''
+        cams_table = self.metadata.tables['cameras']
+        sel = select([cams_table])
+        result = self.connection.execute(sel)
+        return result
+
+##
+## Api resource routing here
+##
+api.add_resource(ParkList, '/parks')
+api.add_resource(Park, '/parks/<park_name>')
+
+handler = DataHandler(database_creds)
+
+if __name__ == '__main__':
+    app.run(debug=True)
