295544c6c75ccf15b65b450bf04b2795d9eca8f9
dayannyc <dayanny.caballero@gmail.com>
2018-03-19 17:00:58 -0500

    Created parkDetail component & moved detailHeader to it's own file

--- react/natphoto/src/components/CameraDetail.js
+++ react/natphoto/src/components/CameraDetail.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
-import { Row, Col } from 'reactstrap';
+import { Row } from 'reactstrap';
 import NavBar from './NavBar.js';
+import DetailHeader from './DetailHeader.js';
 import '../stylesheets/general.css';
 import '../stylesheets/cameradetail.css';
 
@@ -12,7 +13,7 @@ export default class CameraDetail extends Component {
 			<div className="body">
 				<NavBar />
 				<h1 className="header"><span>Canon EOS 5D Mark IV</span></h1>
-				<DetailHeader pic="https://4.img-dpreview.com/files/p/E~TS520x0~articles/9335477559/canon_16_35mm_III_5.jpeg" name="Canon" infoAttributes={cameraLabels}/>
+				<DetailHeader pic="https://4.img-dpreview.com/files/p/E~TS520x0~articles/9335477559/canon_16_35mm_III_5.jpeg" name="Death Valley National Park" infoAttributes={cameraLabels}/>
 
 				<Row id="table-row">
 					<h1 id = "photos">Photos Taken</h1>
@@ -29,39 +30,6 @@ export default class CameraDetail extends Component {
   				<h1 id = "parksListed" >Parks Used At</h1>
 			</div>
 
-		);
-	}
-}
-
-class DetailHeader extends Component {
-	render (){
-		var keys = Object.keys(this.props.infoAttributes);
-
-		return (
-			<Row>
-  				<Col sm="6">
-    				<img className="image" src={this.props.pic} alt={this.props.name}/>
-    			</Col>
-
- 		  		<Col sm="3" id="info-text">
-			  		<h2 id = "infoHeader">Info</h2>
-      				<h3>{keys[0]}</h3>
-      				<h3>{keys[1]}</h3>
-      				<h3>{keys[2]}</h3>
-      				<h3>{keys[3]}</h3>
-      				<h3>{keys[4]}</h3>
-      				<h3>{keys[5]}</h3>
-      			</Col>
-
-      			<Col sm="3" id="ans-text">
-			  		<h3>{this.props.infoAttributes[keys[0]+""]}</h3>
-      				<h3>{this.props.infoAttributes[keys[1]+""]}</h3>
-      				<h3>{this.props.infoAttributes[keys[2]+""]}</h3>
-      				<h3>{this.props.infoAttributes[keys[3]+""]}</h3>
-      				<h3>{this.props.infoAttributes[keys[4]+""]}</h3>
-      				<h3>{this.props.infoAttributes[keys[5]+""]}</h3>
-		  		</Col>
-  			</Row>
 		);
 	}
 }
---
+++ react/natphoto/src/components/DetailHeader.js
@@ -0,0 +1,37 @@
+import React, {Component} from 'react';
+import { Row, Col } from 'reactstrap';
+import '../stylesheets/cameradetail.css';
+
+
+export default class DetailHeader extends Component {
+	render (){
+		var keys = Object.keys(this.props.infoAttributes);
+
+		return (
+			<Row>
+  				<Col sm="6">
+    				<img className="image" src={this.props.pic} alt={this.props.name}/>
+    			</Col>
+
+ 		  		<Col sm="3" id="info-text">
+			  		<h2 id = "infoHeader">Info</h2>
+      				<h3>{keys[0]}</h3>
+      				<h3>{keys[1]}</h3>
+      				<h3>{keys[2]}</h3>
+      				<h3>{keys[3]}</h3>
+      				<h3>{keys[4]}</h3>
+      				<h3>{keys[5]}</h3>
+      			</Col>
+
+      			<Col sm="3" id="ans-text">
+			  		<h3>{this.props.infoAttributes[keys[0]+""]}</h3>
+      				<h3>{this.props.infoAttributes[keys[1]+""]}</h3>
+      				<h3>{this.props.infoAttributes[keys[2]+""]}</h3>
+      				<h3>{this.props.infoAttributes[keys[3]+""]}</h3>
+      				<h3>{this.props.infoAttributes[keys[4]+""]}</h3>
+      				<h3>{this.props.infoAttributes[keys[5]+""]}</h3>
+		  		</Col>
+  			</Row>
+		);
+	}
+}
