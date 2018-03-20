7f25841584bcb28f264c48347d4907787540f113
Benjamin Johnson <bencj96@gmail.com>
2018-03-20 17:33:49 -0500

    broken react api puller

--- react/natphoto/src/components/PhotoGrid.js
+++ react/natphoto/src/components/PhotoGrid.js
@@ -2,12 +2,32 @@ import React, {Component} from "react";
 import Grid from './GridPage.js';
 
 export default class PhotoGrid extends Component {
+  constructor(props){
+    super(props);
+
+    this.state = {
+      list: []
+    };
+
+    fetch('http://api.natphoto.me/photos', {method: 'GET', dataType: 'json'})
+    .then(r => r.json())
+    .then(j => {
+      var i = 0
+      var cur_photos = []
+      while (i < 9){
+        var photo = {img: j[i].image_url, title:j[i].title, subtite:j[i].photographer};
+        cur_photos.push(photo);
+        i++;
+      }
+      console.log(cur_photos);
+      this.setState ({
+        list: cur_photos
+      });
+    });
+  }
+
 	render() {
-		var photo1 = {img:"https://c1.staticflickr.com/5/4529/37718090005_023fa923a2_h.jp", title:"Half A Sunset", subtitle:"Nicolas Jaud"};
-		var photo2 = {img:"https://c1.staticflickr.com/9/8406/8679159813_1f23534aed_b.jpg", title:"Mesquite Flat Dunes", subtitle:"I-Ting Chiang"};
-		var photo3 = {img:"https://c1.staticflickr.com/3/2885/9249507518_25eabef5f3_h.jpg", title:"Yellowstone National Park - 3", subtitle:"Punit Sharma"};
-		var photos = [photo1, photo2, photo3];
-		
+    let photos = this.state.list
 		return (
 			<Grid data={photos} />
 		);
