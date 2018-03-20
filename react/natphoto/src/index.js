4053e5594f35f88f03617959da336a58d64ed0ff
root <vargasbri2@gmail.com>
2018-03-20 18:39:44 +0000

    Park detail page generalized + styling issues fixed

--- react/natphoto/src/index.js
+++ react/natphoto/src/index.js
@@ -1,16 +1,18 @@
 /*eslint no-unused-vars: "off" */
 import React from 'react';
-import { Router, Route } from 'react-router';
-import { BrowserRouter } from 'react-router-dom'
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import 'bootstrap/dist/css/bootstrap.css';
 
-ReactDOM.render(
-	<BrowserRouter>
-		<App />
-	</BrowserRouter>,
-	document.getElementById('root')
-);
+// Our components
+import Home from './components/Home.js'
+import About from './components/About.js'
+import PhotoGrid from './components/gridpage.js'
+import PhotoDetail from './components/photodetailpage.js'
+import CameraDetail from './components/CameraDetail.js'
+import ParkDetail from './components/ParkDetail.js'
+
+ReactDOM.render(<ParkDetail />, document.getElementById('root'));
+registerServiceWorker();
