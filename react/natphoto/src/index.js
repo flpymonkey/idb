04ff9ca52b1de23dba0227eb54fb51040618c500
tonydenapoli <tdenapoli1@gmail.com>
2018-03-20 15:02:44 -0500

    fixed index.js to allow navigation

--- react/natphoto/src/index.js
+++ react/natphoto/src/index.js
@@ -1,18 +1,15 @@
 /*eslint no-unused-vars: "off" */
 import React from 'react';
+import { Router, Route } from 'react-router';
+import { BrowserRouter } from 'react-router-dom'
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import 'bootstrap/dist/css/bootstrap.css';
 
-// Our components
-import Home from './components/Home.js'
-import About from './components/About.js'
-import PhotoGrid from './components/gridpage.js'
-import PhotoDetail from './components/photodetailpage.js'
-import CameraDetail from './components/CameraDetail.js'
-import ParkDetail from './components/ParkDetail.js'
-
-ReactDOM.render(<ParkDetail />, document.getElementById('root'));
+ReactDOM.render(
+	<BrowserRouter>
+		<App />
+	</BrowserRouter>, document.getElementById('root'));
 registerServiceWorker();
