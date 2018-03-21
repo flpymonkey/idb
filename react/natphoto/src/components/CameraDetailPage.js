b64c7aae5927b0a8432c1657edc4d300835b80f5
Jeff Bell <jeff@jhbell.com>
2018-03-21 01:34:32 -0500

    Fill out CameraDetailPage using API data

--- react/natphoto/src/components/CameraDetailPage.js
+++ react/natphoto/src/components/CameraDetailPage.js
@@ -6,14 +6,73 @@ import '../stylesheets/general.css';
 import '../stylesheets/cameradetail.css';
 
 export default class CameraDetail extends Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      effective_megapixels: "",
+      image_resolution: "",
+      image_url: "",
+      iso: "",
+      name: "",
+      price: "",
+      sensor: "",
+      shutter_speeds: "",
+      total_megapixels: "",
+      type: "",
+      video_resolution: "",
+      water_resistant: "",
+      weight: ""
+    }
+  }
+
+  componentDidMount() {
+    fetch('http://api.natphoto.me/cameras/' + this.props.match.params.camera_name, {
+      method: 'GET',
+      dataType: 'json',
+    }).then(results => {
+      return results.json();
+    }).then(data => {
+      console.log(data['0'])
+      this.setState({
+          effective_megapixels: data['0'].effective_megapixels,
+          image_resolution: data['0'].image_resolution,
+          image_url: data['0'].image_url,
+          iso: data['0'].iso,
+          name: data['0'].name,
+          price: data['0'].price,
+          sensor: data['0'].sensor,
+          shutter_speeds: data['0'].shutter_speeds,
+          total_megapixels: data['0'].total_megapixels,
+          type: data['0'].type,
+          video_resolution: data['0'].video_resolution,
+          water_resistant: data['0'].water_resistant,
+          weight: data['0'].weight
+      });
+    })
+  }
+
 	render (){
-		var cameraLabels = {'Brand:':"Canon", 'Model:':"EOS 5D Mark IV", 'Max Resolution:':"6720x4480", 'Effective Pixels:':"30 megapixels", 'Continuous Drive Speed:':"7.0 fps", 'Video Enabled:':"Yes"};
+		var cameraLabels = {
+        "Price": this.state.price,
+        "Camera Type": this.state.type,
+        "Effective Megapixels": this.state.effective_megapixels,
+        "Total Megapixels": this.state.total_megapixels,
+        "Image Resolution": this.state.image_resolution,
+        "Video Resolution": this.state.video_resolution,
+        "Shutter Speeds": this.state.shutter_speeds,
+        "ISO": this.state.iso,
+        "Water Resistant": this.state.water_resistant,
+        "Weight": this.state.weight,
+        "Sensor": this.state.sensor
+    }
 
 		return (
 			<div className="body">
 				<NavBar />
-				<h1 className="cameraHeader"><span>Canon EOS 5D Mark IV</span></h1>
-				<DetailHeader pic="https://4.img-dpreview.com/files/p/E~TS520x0~articles/9335477559/canon_16_35mm_III_5.jpeg" name="Death Valley National Park" infoAttributes={cameraLabels}/>
+				<h1 className="cameraHeader"><span>{this.state.name}</span></h1>
+				<DetailHeader pic={this.state.image_url} name={this.state.name} infoAttributes={cameraLabels}/>
 
 				<Row id="table-row">
 					<h1 id = "photos">Photos Taken</h1>
