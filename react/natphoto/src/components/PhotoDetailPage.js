6d6fb0144eda9bc5d7ee370f4e0b688cf824cbdf
Benjamin Johnson <bencj96@gmail.com>
2018-03-22 01:00:55 -0500

    styling PhotoDetailPage

--- react/natphoto/src/components/PhotoDetailPage.js
+++ react/natphoto/src/components/PhotoDetailPage.js
@@ -50,18 +50,19 @@ export default class PhotoDetail extends Component {
 		return (
 			<div className="body">
 				<Row>
-					<Col sm="12" id="title">
-  						<h1>{this.state.title}</h1>
-  					</Col>
+          <Col sm="4"/>
+					<Col sm="4" id="title">
+  						<h5>{this.state.title}</h5>
+  				</Col>
+
 				</Row>
 				<Row>
-					<Col sm="1"></Col>
-					<Col sm="10" id="photo">
+					<Col xs="1" sm="3"></Col>
+					<Col xs="10" sm="5" id="photo">
             <a href={this.state.flickr_url} target="_blank">
 						  <img id="picture" src={this.state.image_url} alt={this.state.title}  />
             </a>
 					</Col>
-					<Col sm="1"></Col>
 				</Row>
 				<Row>
 					<Col sm="12" id="title">
