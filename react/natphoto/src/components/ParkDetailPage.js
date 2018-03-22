08d8394963eaec8b322466b6165a6574cf59509f
Benjamin Johnson <bencj96@gmail.com>
2018-03-22 00:41:24 -0500

    Added ParkDetailPage, website is FULLY FUNCTIONAL WEBSITE NOW! YAY

--- react/natphoto/src/components/ParkDetailPage.js
+++ react/natphoto/src/components/ParkDetailPage.js
@@ -17,7 +17,9 @@ export default class ParkDetail extends Component {
         name: "",
         states: "",
         url: "",
-        weather: ""
+        weather: "",
+        photos: [],
+        cameras: []
       };
     }
 
@@ -41,6 +43,34 @@ export default class ParkDetail extends Component {
           weather: data["0"].weather
         });
       })
+
+      fetch('http://api.natphoto.me/photos?park=' + this.props.match.params.park_name, {
+        method: 'GET',
+        dataType: 'json',
+      }).then(results => {
+        return results.json();
+      }).then(data => {
+          var curr_photos = data.map((elem) => ({
+                img: elem.image_url, path: "/photos/" + elem.id, name: elem.title}
+            ));
+          this.setState({
+            photos: curr_photos
+          });
+      });
+
+      fetch('http://api.natphoto.me/cameras?park=' + this.props.match.params.park_name, {
+        method: 'GET',
+        dataType: 'json',
+      }).then(results => {
+        return results.json();
+      }).then(data => {
+          var curr_cameras = data.map((elem) => ({
+                img: elem.image_url, path: "/cameras/" + elem.name, name: elem.name}
+            ));
+          this.setState({
+            cameras: curr_cameras
+          });
+      });
   	}
 
 	render() {
@@ -51,8 +81,8 @@ export default class ParkDetail extends Component {
 				<h1 className="parkHeader"><span>{this.state.name}</span></h1>
 				<DetailHeader pic={this.state.image_url} name={this.state.name} infoAttributes={parkLabels}/>
 
-				<ScrollableTable tableTitle="Photos Taken" />
-				<ScrollableTable tableTitle="Cameras Used" />
+				<ScrollableTable tableTitle="Photos Taken" data={this.state.photos} />
+        <ScrollableTable tableTitle="Cameras Used" data={this.state.cameras} />
 
 			</div>
 		);
