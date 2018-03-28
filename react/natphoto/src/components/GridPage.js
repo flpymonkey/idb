fa7efd1ebba383611ecf76e5291a4fd6f161e86f
Jeff Bell <jeff@jhbell.com>
2018-03-28 17:22:36 -0500

    Add some beautiful spinners while images load
    Close #90

--- react/natphoto/src/components/GridPage.js
+++ react/natphoto/src/components/GridPage.js
@@ -4,10 +4,9 @@ import { Row, Col, Container} from 'reactstrap';
 import Pagination from 'react-js-pagination';
 import '../stylesheets/gridpage.css';
 import '../stylesheets/general.css';
-
+import { SyncLoader } from 'react-spinners';
 import { Card, CardImg, CardText, CardBody,
   CardTitle, CardSubtitle } from 'reactstrap';
-
 
 
 export default class Grid extends Component {
@@ -56,13 +55,48 @@ export default class Grid extends Component {
 }
 
 class GridItemCard extends Component {
+
+  constructor(props) {
+    super(props)
+    this.state = { loading: true };
+  }
+
+  handleLoad(image) {
+      this.setState({ loading: false });
+  }
+
+  // Wait until the image is loaded, then display it. Display a spinner in
+  // its place when it is still loading.
+  renderImage() {
+    if (this.state.loading) {
+      return (
+        <div>
+        <Row>
+          <div className="col-xs-6 col-xs-offset-3 spinner">
+            <SyncLoader color={"#009d00"} size={10} margin={"5px"} />
+          </div>
+        </Row>
+        <CardImg className="hidden"
+             src={this.props.data.img} 
+             alt="Card image cap" onLoad={this.handleLoad.bind(this)} />
+        </div>
+      );
+    } else {
+      return (
+        <CardImg className="imgInCard"
+             src={this.props.data.img} 
+             alt="Card image cap" onLoad={this.handleLoad.bind(this)} />
+      );
+    }
+  }
+
   render() {
       return(
         <Col xl="3" lg="3" md="3" sm="3">
           <div className="cardDiv">
             <Card id={this.props.data.detail_url}>
               <Link to={this.props.data.detail_url}>
-              <CardImg className="imgInCard" src={this.props.data.img} alt="Card image cap" />
+              {this.renderImage()}
               <CardBody>
                 <CardTitle>{this.props.data.title}</CardTitle>
                 <CardSubtitle>{this.props.data.subtitle}</CardSubtitle>
