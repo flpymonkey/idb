a26b928317cf9d2ed571d5bc5022e26e3c1e83be
Jeff Bell <jeff@jhbell.com>
2018-04-04 17:42:21 -0500

    Carousel sizing and repeating photo fixed

--- react/natphoto/src/components/Carousel.js
+++ react/natphoto/src/components/Carousel.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import { Carousel } from 'react-responsive-carousel'
+import '../stylesheets/carousel.css';
 
 import 'react-responsive-carousel/lib/styles/carousel.min.css'
 import 'bootstrap/dist/css/bootstrap.css'
@@ -15,7 +16,7 @@ export class MyCarousel extends Component {
                     <img className="carousel-img" src="https://photos.smugmug.com/North-America/California/Redwood-National-Park/i-fQqHSJc/0/f5692c09/X2/_GMA7647_tonemapped-X2.jpg" alt=""/>
                 </div>
                 <div>
-                    <img className="carousel-img" src="https://photos.smugmug.com/North-America/California/Redwood-National-Park/i-fQqHSJc/0/f5692c09/X2/_GMA7647_tonemapped-X2.jpg" alt=""/>
+                    <img className="carousel-img" src="http://www.urbancapture.com/wp-content/uploads/yapb_cache/130414_1656_waterfalls_igua__u_national_park_brazil.5c756plwtyosk04wkcgsccwco.8hrw0xj94288kk84gsowgs4o4.th.jpeg" alt=""/>
                 </div>
             </Carousel>
         )
