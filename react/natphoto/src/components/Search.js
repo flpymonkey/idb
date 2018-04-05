fb51daba678bee0faf71c58252f1a93d74ccb967
dayannyc <dayanny.caballero@gmail.com>
2018-04-04 21:43:50 -0500

    Pagination on search page

--- react/natphoto/src/components/Search.js
+++ react/natphoto/src/components/Search.js
@@ -2,6 +2,7 @@ import React, {Component} from "react";
 import { Col, Row, Container } from 'reactstrap';
 import SearchItem from './SearchItem.js'
 import { SyncLoader } from 'react-spinners';
+import Pagination from 'react-js-pagination';
 
 import '../stylesheets/search.css';
 
@@ -19,7 +20,8 @@ export default class Search extends Component {
         search_string: parsed['q'],
         search_results: [],
         loading: true,
-        numResults: 0
+        numResults: 0,
+        activePage: 0
       };
 
       this.renderResults = this.renderResults.bind(this);
@@ -61,8 +63,14 @@ export default class Search extends Component {
         })
     }
 
+    handlePageChange(pageNumber) {
+        this.setState({activePage: pageNumber});
+    }
+
     // Render the search results OR a spinner if the results are still loading.
     renderResults() {
+      let endVal = (this.state.activePage * 5)
+      let startVal = ((this.state.activePage - 1) * 5)
       if (this.state.loading) {
         return (
           <Col className="loader">
@@ -77,13 +85,22 @@ export default class Search extends Component {
           return ("No results found.");
         } else {
           return (
-            <div>
+            <div className="searchResults">
               <Row>
                 <Col>
                   <h2>{this.state.numResults} results found for "{this.state.search_string}"</h2>
                 </Col>
               </Row>
-              <div>{this.state.results}</div>
+              <div>{this.state.results.slice(startVal, endVal)}</div>
+              <Pagination
+                activePage={this.state.activePage}
+                itemsCountPerPage={5}
+                totalItemsCount={this.state.numResults}
+                pageRangeDisplayed={5}
+                onChange={this.handlePageChange.bind(this)}
+                className = "pagination"
+              />
+              <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"/>
             </div>
           )
         }
