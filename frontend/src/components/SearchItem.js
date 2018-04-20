0e77a875c082ded5e144e6a8a08712f51bd42152
Benjamin Johnson <bencj96@gmail.com>
2018-04-19 19:11:29 -0500

    Refactored SearchItem.js

--- frontend/src/components/SearchItem.js
+++ frontend/src/components/SearchItem.js
@@ -8,104 +8,50 @@ const SEARCH_RANGE = 100;
 export default class SearchItem extends Component {
   constructor(props) {
     super(props);
-    var link = '';
-    var header = '';
-    var park = '';
-    var camera = '';
-    var description = '';
-    var weather = '';
-    var directions = '';
-    var states = '';
-    var likes = '';
-    var photographer = '';
-    var date = '';
-    // camera search stuff
-    var price = '';
-    var weight = '';
-    var effective_megapixels = '';
-    var total_megapixels = '';
-    var shutter_speeds = '';
-    var iso = '';
-    var water_resistant = '';
-    var sensor = '';
-    var type = '';
+    var search_item_data = {};
+    var header = "";
+    var link = "";
     if (this.props.data.id) {
       header = 'Photo: ' + this.props.data.title;
-      park = this.props.data.park;
-      camera = this.props.data.camera;
-      description = this.props.data.description;
       link = '/photos/' + this.props.data.id;
-      likes = this.props.data.likes;
-      photographer = this.props.data.photographer;
-      date = this.props.data.date;
+      search_item_data["park"] = this.props.data.park;
+      search_item_data["camera"] = this.props.data.camera;
+      search_item_data["description"] = this.props.data.description;
+      search_item_data["likes"] = this.props.data.likes;
+      search_item_data["photographer"] = this.props.data.photographer;
+      search_item_data["date"] = this.props.data.date;
     } else if (
       this.props.data.name !== null &&
       this.props.data.name.indexOf('National Park') !== -1
     ) {
       header = 'Park: ' + this.props.data.name;
-      description = this.props.data.description;
       link = '/parks/' + this.props.data.name;
-      weather = this.props.data.weather;
-      directions = this.props.data.directions;
-      states = this.props.data.states;
+      search_item_data["description"] = this.props.data.description;
+      search_item_data["weather"] = this.props.data.weather;
+      search_item_data["directions"] = this.props.data.directions;
+      search_item_data["states"] = this.props.data.states;
     } else {
       header = 'Camera: ' + this.props.data.name;
       link = '/cameras/' + this.props.data.name;
-      price = this.props.data.price;
-      type = this.props.data.type;
-      weight = this.props.data.weight;
-      effective_megapixels = this.props.data.effective_megapixels;
-      total_megapixels = this.props.data.total_megapixels;
-      shutter_speeds = this.props.data.shutter_speeds;
-      iso = this.props.data.iso;
-      water_resistant = this.props.data.water_resistant;
-      sensor = this.props.data.sensor;
+      search_item_data["price"] = this.props.data.price;
+      search_item_data["type"] = this.props.data.type;
+      search_item_data["weight"] = this.props.data.weight;
+      search_item_data["effective_megapixels"] = this.props.data.effective_megapixels;
+      search_item_data["total_megapixels"] = this.props.data.total_megapixels;
+      search_item_data["shutter_speeds"] = this.props.data.shutter_speeds;
+      search_item_data["iso"] = this.props.data.iso;
+      search_item_data["water_resistant"] = this.props.data.water_resistant;
+      search_item_data["sensor"] = this.props.data.sensor;
     }
     this.state = {
       header: header,
-      park: park,
-      camera: camera,
-      description: description,
       link: link,
       img_url: this.props.data.image_url,
-      weather: weather,
-      states: states,
-      directions: directions,
-      photographer: photographer,
-      date: date,
-      type: type,
-      likes: likes,
-      price: price,
-      weight: weight,
-      effective_megapixels: effective_megapixels,
-      total_megapixels: total_megapixels,
-      shutter_speeds: shutter_speeds,
-      iso: iso,
-      water_resistant: water_resistant,
-      sensor: sensor,
-      headers: [
-        'park',
-        'camera',
-        'description',
-        'weather',
-        'type',
-        'states',
-        'directions',
-        'photographer',
-        'date',
-        'likes',
-        'price',
-        'weight',
-        'effective_megapixels',
-        'total_megapixels',
-        'shutter_speeds',
-        'iso',
-        'water_resistant',
-        'sensor'
-      ]
+      data : search_item_data
     };
   }
 
+  /* Truncates search results for better formatting */
   truncate(searchTerm, val) {
     if (val.length <= SEARCH_RANGE * 2) {
       return val;
@@ -125,10 +71,11 @@ export default class SearchItem extends Component {
     return result;
   }
 
+  /* Used to only show attirbutes of search items which include search terms */
   getModelAttributes(highlightWords) {
-    return this.state.headers.map(
+    return Object.keys(this.state.data).map(
       function(elem, i) {
-        var searchPhrase = this.state[elem].replace(/<.*?>/g, '');
+        var searchPhrase = this.state.data[elem].replace(/<.*?>/g, '');
         for (let word of highlightWords) {
           var indexOfWord = searchPhrase
             .toLowerCase()
